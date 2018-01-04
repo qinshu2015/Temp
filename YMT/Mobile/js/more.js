e825e3096bc48c30f457b3a8438d46d13ca144e6
qinshu2015 <13110295067@163.com>
2018-01-04 10:43:50 +0800

    Add files via upload

--- YMT/Mobile/js/more.js
+++ YMT/Mobile/js/more.js
@@ -1,11 +1,11 @@
 $(function(){
 			$(".btnshow").toggle(function(){
 				//alert();
-				$(this).parents(".aboutpush").find(".texthide").slideDown();
+				$(this).parents(".Morepull").find(".texthide").slideDown();
 				$(this).html("收起")
 			},function(){
 				//alert(1);
-				$(this).parents(".aboutpush").find(".texthide").slideUp();
+				$(this).parents(".Morepull").find(".texthide").slideUp();
 				$(this).html("更多")
 			});
 		})
